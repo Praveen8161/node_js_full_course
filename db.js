9f560d969f0b51cc9c067adb2bdc886b6847c11e
Praveen8161 <rbalu8161@gmail.com>
2023-11-28 09:57:25 +0530

    updated

--- db.js
+++ db.js
@@ -14,7 +14,7 @@ const localString = process.env.LOCAL_URL ;
 
 async function dbConnection(){
     try{
-        const client = new MongoClient(localString);
+        const client = new MongoClient(connectionString);
         await client.connect();
         console.log("Database Connected");
         return client;
